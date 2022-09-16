ee16da21133dbea27dc4d2f1cbc36093d3becb86
Wangari Gichina <ruthgichina@gmail.com>
2022-09-15 19:20:01 -0700

    C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -15,13 +15,13 @@ _putchar('\n');
 } else
 {
 for (i = 0; i < n; i++)
-	{
-	for (j = 0; j < i; j++)
-	{
-	_putchar(' ');
-	}
-	_putchar('\\');
-	_putchar('\n');
-	}
+{
+for (j = 0; j < i; j++)
+{
+_putchar(' ');
+}
+_putchar('\\');
+_putchar('\n');
+}
 }
 }
