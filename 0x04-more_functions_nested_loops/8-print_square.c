cb987726bd4b8465cd1e97877c63cf213be2d1af
Wangari Gichina <ruthgichina@gmail.com>
2022-09-15 20:00:40 -0700

    C - More functions, more nested loops

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -15,6 +15,7 @@ _putchar('\n');
 } 
 else
 {
+
 for (i = 0; i < size; i++)
 {
 for (j = 0; j < size; j++)
@@ -23,5 +24,7 @@ _putchar('#');
 }
 _putchar('\n');
 }
+
 }
+
 }
--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -11,12 +11,14 @@ int i;
 
 for (i = 1; i <= 100; i++)
 {
+  
 if (i % 3 == 0 || i % 5 == 0)
 {
 if (i % 3 == 0)
 {
 printf("Fizz");
 }
+ 
 if (i % 5 == 0)
 {
 printf("Buzz");
@@ -26,9 +28,12 @@ else
 {	
 printf("%d", i);
 }
+ 
 if (i != 100)
+{
 printf(" ");
 }
+} 
 printf("\n");
 return (0);
 }
